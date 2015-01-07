c84e299ec121275d126333eb05f2817313823271
athoo <athoo@sina.com>
2015-01-08 00:12:10 +0800

    testing

--- movie_worker.rb
+++ movie_worker.rb
@@ -1,2 +1,8 @@
-puts "Starting movie_worker at #{Time.now}"
+require "movie_crawler"
 
+pp "this is a test for worker"
+pp MovieCrawler::us_weekend
+pp MovieCrawler::taipei_weekend
+pp MovieCrawler::dvd_rank
+pp MovieCrawler::movies("latest")
+pp MovieCrawler::movies("second_round")
